42eb95a90fe01fb8b84487fb450d52da93270014
YogSottot <yogsottot@boson.nom.za>
2018-04-20 10:10:31 +0300

    added some postfix interactive

--- initial_server_setup/postfix.sh
+++ initial_server_setup/postfix.sh
@@ -9,7 +9,7 @@ yum install cyrus-sasl-plain postfix -y
 # postfix limits
 mkdir -p /etc/systemd/system/postfix.service.d && echo -e '[Service]\nLimitNPROC=65535\nLimitNOFILE=100000'  >> /etc/systemd/system/postfix.service.d/override.conf && systemctl daemon-reload
 
-cat <<EOT >> /etc/postfix/main.cf
+cat <<\EOT >> /etc/postfix/main.cf
 #default_destination_concurrency_limit = 500
 #default_destination_recipient_limit = 500
 #initial_destination_concurrency = 50
@@ -27,3 +27,62 @@ systemctl enable postfix && systemctl restart postfix
 cat <<EOT >> /etc/php.d/z_bx_custom.ini
 sendmail_path = sendmail -t -i
 EOT
+
+echo "Do you wish to setup relay?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+
+cat <<EOT >> /etc/postfix/main.cf
+#inet_protocols = ipv4
+smtp_sasl_auth_enable = yes
+smtp_sasl_password_maps = hash:/etc/postfix/mailpasswd
+smtp_sasl_security_options = noanonymous
+smtp_sasl_type = cyrus
+smtp_sasl_mechanism_filter = login
+smtp_sender_dependent_authentication = yes
+header_size_limit = 4096000
+smtp_use_tls = yes
+sender_dependent_relayhost_maps = hash:/etc/postfix/sender_relay
+sender_canonical_maps = hash:/etc/postfix/canonical
+smtp_generic_maps = hash:/etc/postfix/generic
+EOT
+
+read -p "Please enter your relay. Example: smtp.yandex.ru: " relay
+read -p "Please enter relay port. Example: 587: " port
+read -p "Please enter mail login. Example: www@some.ru: " login
+read -p "Please enter mail password: " password
+read -p "Please enter mail domain. Example: some.ru: " domain
+
+# указываем логин и пароль
+cat <<EOT >> /etc/postfix/mailpasswd
+[${relay}]:${port}      ${login}:${password}
+EOT
+
+# указываем привязку доменов и конкретных отправителей к внешним службам
+cat <<EOT >> /etc/postfix/sender_relay
+@${domain} [${relay}]:${port}
+info@${domain} [${relay}]:${port}
+EOT
+
+# добавляем для домена указание через какой аккаунт отправлять
+cat <<EOT >> /etc/postfix/canonical
+@${domain}   info@${domain}
+EOT
+
+# добавляем отправку почты админу
+cat <<EOT >> /etc/postfix/generic
+root@localhost postmaster@${domain}
+bitrix@localhost postmaster@${domain}
+EOT
+
+postmap /etc/postfix/generic
+postmap /etc/postfix/canonical
+postmap /etc/postfix/sender_relay
+postmap /etc/postfix/mailpasswd
+
+systemctl restart postfix
+echo 'relay setup is succes' ; break;;
+        No ) exit;;
+    esac
+done
