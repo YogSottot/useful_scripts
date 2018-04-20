ed9ca83eb84000d8705cfd06f297d5a8f5175098
YogSottot <yogsottot@boson.nom.za>
2018-04-20 17:22:24 +0300

    added rclone script

--- bitrix/rclone.sh
+++ bitrix/rclone.sh
@@ -8,13 +8,11 @@
 # 4 - supported unzip tools are not available
 
 # use curl -sL https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/rclone.sh | bash
-
+yum install unzip -y
 set -e
 
 #when adding a tool to the list make sure to also add it's corresponding command further in the script
 unzip_tools_list=('unzip' '7z')
-
-yum install unzip -y
 
 usage() { echo "Usage: curl https://rclone.org/install.sh | sudo bash [-s beta]" 1>&2; exit 1; }
 
