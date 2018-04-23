b23188c7679eebf71e413823403afa774616a567
YogSottot <yogsottot@boson.nom.za>
2018-04-23 17:13:18 +0300

    fix saving abh

--- av/update.abh.sh
+++ av/update.abh.sh
@@ -4,7 +4,7 @@ path="/opt/av/abh/"
 if [ ! -d  ${path} ]; then mkdir -p ${path}; fi
 
 rm -f ${path}ai.zip
-wget -P ${path} http://bit.ly/2GRX3zL -O ai.zip
+wget http://bit.ly/2GRX3zL -O /opt/av/abh/ai.zip
 unzip -o ${path}ai.zip -d $path
 
 exit 0
