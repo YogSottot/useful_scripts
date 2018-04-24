d8b95762df7b23c4ab38fe029fd458efc0ad0e79
YogSottot <yogsottot@boson.nom.za>
2018-04-24 13:18:04 +0300

    added brackets

--- isp/kill_semaphores.sh
+++ isp/kill_semaphores.sh
@@ -13,14 +13,14 @@ IPCS_S=$(ipcs -s | egrep "0x[0-9a-f]+ [0-9]+" | grep ${USER} | cut -f2 -d" ")
 IPCS_M=$(ipcs -m | egrep "0x[0-9a-f]+ [0-9]+" | grep ${USER} | cut -f2 -d" ")
 IPCS_Q=$(ipcs -q | egrep "0x[0-9a-f]+ [0-9]+" | grep ${USER} | cut -f2 -d" ")
 
-for id in $IPCS_M; do
-  ipcrm -m $id;
+for id in ${IPCS_M}; do
+  ipcrm -m ${id};
 done
 
 for id in $IPCS_S; do
-  ipcrm -s $id;
+  ipcrm -s ${id};
 done
 
 for id in $IPCS_Q; do
-  ipcrm -q $id;
+  ipcrm -q ${id};
 done
