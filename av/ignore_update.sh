fe88b2188727c8af919d6a157f36ed46b928e967
YogSottot <yogsottot@boson.nom.za>
2018-04-24 12:59:07 +0300

    fixed ignore_updater

--- av/ignore_update.sh
+++ av/ignore_update.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 path="/opt/av/"
-wget https://raw.githubusercontent.com/YogSottot/useful_scripts/master/av/.adirignore
-wget https://raw.githubusercontent.com/YogSottot/useful_scripts/master/av/.aurlignore
-wget https://raw.githubusercontent.com/YogSottot/useful_scripts/master/av/.aignore
+wget https://raw.githubusercontent.com/YogSottot/useful_scripts/master/av/.adirignore -O ${path}.adirignore
+wget https://raw.githubusercontent.com/YogSottot/useful_scripts/master/av/.aurlignore -O ${path}.aignore
+wget https://raw.githubusercontent.com/YogSottot/useful_scripts/master/av/.aignore -O ${path}.aurlignore
 
 cat ${path}.adirignore >> ${path}abh/ai-bolit/.adirignore
 cat ${path}.adirignore_usr >> ${path}abh/ai-bolit/.adirignore
