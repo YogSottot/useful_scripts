619765849f787511cbd574a0dce3c1dba8810766
YogSottot <yogsottot@boson.nom.za>
2018-04-24 13:17:24 +0300

    added brackets

--- isp/kvm_vnet_reattach.sh
+++ isp/kvm_vnet_reattach.sh
@@ -6,7 +6,7 @@
 # if you run service network restart on vmmanager, all vnetX is not reattached
 vnet=$( ip a | grep vnet* | awk '{print substr($2, 0, length($2)-1)}' )
 
-ip link set dev $vnet master vmbr0
+ip link set dev ${vnet} master vmbr0
 #or
 #brctl addif vmbr0 $vnet
 #ip link set $vnet up
