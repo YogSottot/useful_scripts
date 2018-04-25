07ae601fb2498a535143e56ff4b192b06bbd03cf
YogSottot <yogsottot@boson.nom.za>
2018-04-25 16:14:25 +0300

    changed location of the install-ngxblocker

--- av/botblock.sh
+++ av/botblock.sh
@@ -8,7 +8,7 @@
 yum -y install bind-utils
 wget https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/install-ngxblocker -O /usr/local/sbin/install-ngxblocker
 chmod +x /usr/local/sbin/install-ngxblocker
-/usr/sbin/install-ngxblocker -c /etc/nginx/bx/settings -x -s /usr/sbin/
+/usr/sbin/install-ngxblocker -c /etc/nginx/bx/settings -x
 setup-ngxblocker -v /etc/nginx/bx/site_avaliable -c /etc/nginx/bx/settings -e conf -x
 
 cat <<EOT >>/etc/nginx/bots.d/blacklist-user-agents.conf
