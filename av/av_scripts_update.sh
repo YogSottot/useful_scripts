206b69b00eb22e0fc888481f139cd43f3ae3a802
YogSottot <yogsottot@boson.nom.za>
2018-04-25 08:46:08 +0300

    avoid duplicate of ignore files

--- av/av_scripts_update.sh
+++ av/av_scripts_update.sh
@@ -10,9 +10,9 @@ wget -q https://raw.githubusercontent.com/YogSottot/useful_scripts/master/av/.ad
 wget -q https://raw.githubusercontent.com/YogSottot/useful_scripts/master/av/.aurlignore -O ${path}.aignore
 wget -q https://raw.githubusercontent.com/YogSottot/useful_scripts/master/av/.aignore -O ${path}.aurlignore
 
-cat ${path}.adirignore >> ${path}abh/ai-bolit/.adirignore
+cat ${path}.adirignore > ${path}abh/ai-bolit/.adirignore
 cat ${path}.adirignore_usr >> ${path}abh/ai-bolit/.adirignore
-cat ${path}.aignore >> ${path}abh/ai-bolit/.aignore
+cat ${path}.aignore > ${path}abh/ai-bolit/.aignore
 cat ${path}.aignore_usr >> ${path}abh/ai-bolit/.aignore
 cat ${path}.aurlignore >> ${path}abh/ai-bolit/.aurlignore
 cat ${path}.aurlignore_usr >> ${path}abh/ai-bolit/.aurlignore
--- av/update.abh.sh
+++ av/update.abh.sh
@@ -4,6 +4,7 @@ path="/opt/av/abh/"
 if [ ! -d  ${path} ]; then mkdir -p ${path}; fi
 
 rm -f ${path}ai.zip
+rm -rf ${path}ai-bolit
 wget http://bit.ly/2GRX3zL -O /opt/av/abh/ai.zip
 unzip -o ${path}ai.zip -d $path
 
