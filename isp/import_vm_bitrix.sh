c3d0075c7f2506d986d1768d78c76dd9a2cf2e11
YogSottot <yogsottot@boson.nom.za>
2018-04-26 16:23:29 +0300

    import vm with bitrix-env

---
+++ isp/import_vm_bitrix.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# use curl -sL https://raw.githubusercontent.com/YogSottot/useful_scripts/master/av/av_setup.sh | bash -s -- old_ip new_ip
+# после импорта нужно поменять старый ip на новый не только в /etc/sysconfig/network-scripts/ifcfg-eth0
+old_ip=$1
+new_ip=$2
+
+find /opt/webdir/ -type f -print0 | xargs -0 sed -i "s/${old_ip}/${new_ip}/g"
+find /etc/ansible/ -type f -print0 | xargs -0 sed -i "s/${old_ip}/${new_ip}/g"
+find /etc/hosts -type f -print0 | xargs -0 sed -i "s/${old_ip}/${new_ip}/g"
