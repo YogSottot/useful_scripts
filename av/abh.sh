8dd81e4d520e36fcdffa817d0f099e7e78347590
YogSottot <yogsottot@boson.nom.za>
2018-05-15 09:02:56 +0300

    fix cleaning old reports ab

--- av/abh.sh
+++ av/abh.sh
@@ -15,6 +15,9 @@ if [ ! -d ${path}reports/ ] ; then mkdir -p ${path}reports/; fi
 output=`nice -n 19 ionice -c2 -n7 /usr/bin/php ${path}ai-bolit/ai-bolit-hoster.php --report=${path}reports/REPORT-@DATE@.html --mode=1 --path=/home/bitrix/`
 exitcode=$?
 
+# clean reports older then 14 day
+cleanrep=`find ${path}reports/ -type f -mtime +14 -exec rm {} \;`
+
 # got recent report
 for file in ${path}reports/*; do
   [[ ${file} -nt ${recent_report} ]] && recent_report=${file}
@@ -22,18 +25,18 @@ done
 
 # output
 if [ "${exitcode}" = "0" ]; then
+    ${cleanrep}
     exit 0
 fi
 
 if [ "${exitcode}" = "1" ]; then
     mailx -s "Virus detected on ${hostname}" ${mail} < ${recent_report}
+    ${cleanrep}
 fi
 
 if [ "${exitcode}" = "2" ]; then
     mailx -s "Virus detected on ${hostname}" ${mail} < ${recent_report}
+    ${cleanrep}
 fi
-
-# delete older then 14 day reports
-find ${path}reports/ -type f -mtime +14 -exec rm {} \;
 
 exit 0
