da64c7a336647ab9df5c2752aadd73cebbc29f67
YogSottot <yogsottot@boson.nom.za>
2018-05-16 13:23:41 +0300

    fix errors

--- bitrix/restore_upload.sh
+++ bitrix/restore_upload.sh
@@ -22,13 +22,13 @@ function getValueFromINI2() {
         echo $(echo "$sourceData" | sed -n '/^'${paramName}'\ =\(.*\)$/s//\1/p'  | tr -d "\r" | tr -d "\n" | tr -d "/");
 }
 
-sectionContent=$(sed -n '/^\[cloud\]/,/^\[/p' /opt/backup/config.ini | sed -e '/^\[/d' | sed -e '/^$/d');
+sectionContent=$(sed -n '/^\[cloud\]/,/^\[/p' /opt/sMonit/config.ini | sed -e '/^\[/d' | sed -e '/^$/d');
 login=$(getValueFromINI "$sectionContent" "login");
 userkey=$(getValueFromINI "$sectionContent" "password");
 storage_dir=$(getValueFromINI2 "$sectionContent" "dir");
 
 # to restore with bitrix owner
 sudo -i -u bitrix bash << EOF
-cd ${doc_root} && nice -n 19 ionice -c2 -n7 ~/.local/bin/swift -A https://auth.selcdn.ru -U ${login} -K ${userkey} download --skip-identical ${storage_dir} -p upload && echo OK && exit
+cd ${doc_root} && nice -n 19 ionice -c2 -n7 ~/.local/bin/swift -A https://auth.selcdn.ru -U ${login} -K ${userkey} download --skip-identical ${storage_dir} -p ${doc_root}/upload && echo OK && exit
 echo Error
 EOF
