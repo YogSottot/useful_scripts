42bc3ed4e154c3d69fcb70df4d35704dcb9a0307
YogSottot <yogsottot@boson.nom.za>
2018-06-14 20:26:11 +0300

    backups configs

--- bitrix/backup_configs.sh
+++ bitrix/backup_configs.sh
@@ -13,8 +13,6 @@ fi
 
 /usr/bin/tar cfp - -C /etc/ nginx/ | gzip -c > ${backup_dir}/nginx.tgz
 /usr/bin/tar cfp - -C /etc/ httpd/ | gzip -c > ${backup_dir}/httpd.tgz
-cd ${doc_root} && /usr/bin/tar --exclude './bitrix' --exclude './upload' --exclude './zabbix' --exclude './log_composite.txt' --exclude './demo' --exclude './test_widget' 
---exclude './economy_card' --exclude './libs' --exclude './images' -zcf ./bitrix/backup/missing.files.tgz .
 /usr/bin/tar cfp - -C /etc/ php.d/ | gzip -c >${backup_dir}/phpd.tgz
 /usr/bin/tar cfp - -C /etc/ mysql/ | gzip -c > ${backup_dir}/mysql.tgz
 /usr/bin/tar cfp - -C /var/spool/ cron/ | gzip -c > ${backup_dir}/cron.tgz
