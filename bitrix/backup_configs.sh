6b4d43088ca52363cedc7ec2dd092b81d94689b7
YogSottot <yogsottot@boson.nom.za>
2018-06-14 20:28:29 +0300

    backups configs

--- bitrix/backup_configs.sh
+++ bitrix/backup_configs.sh
@@ -17,4 +17,7 @@ fi
 /usr/bin/tar cfp - -C /etc/ mysql/ | gzip -c > ${backup_dir}/mysql.tgz
 /usr/bin/tar cfp - -C /var/spool/ cron/ | gzip -c > ${backup_dir}/cron.tgz
 
+# for site links
+# /usr/bin/tar cfp - -C /home/bitrix/ext_www/ dev.domain.tld | gzip -c > ${backup_dir}/dev.domain.tld_`date +\%Y.\%m.\%d-\%H.\%M`.tgz > /dev/null 2>&1 
+
 
