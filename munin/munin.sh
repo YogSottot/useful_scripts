4a26b9d9de7f247ecd5eaa699d87d58464e161a4
YogSottot <yogsottot@boson.nom.za>
2018-10-25 12:27:51 +0300

    Fix munin memcahe-plugin name. Avoid overwrite on upgrade package

--- munin/munin.sh
+++ munin/munin.sh
@@ -7,9 +7,10 @@
 mkdir -p /etc/systemd/system/munin-node.service.d && echo -e '[Service]\nRestart=on-failure\nLimitNPROC=65535\nLimitNOFILE=100000\nLimitSTACK=infinity\nPrivateTmp=false' >> /etc/systemd/system/munin-node.service.d/override.conf && systemctl daemon-reload 
 
 # plugin support unix-socket
-wget https://raw.githubusercontent.com/mhwest13/Memcached-Munin-Plugin/master/memcached_ -N -P /usr/share/munin/plugins/
+wget https://raw.githubusercontent.com/mhwest13/Memcached-Munin-Plugin/master/memcached_ -N -O /usr/share/munin/plugins/memcached2_
+chmod +x /usr/share/munin/plugins/memcached2_
 
-ln -s '/usr/share/munin/plugins/memcached_' '/etc/munin/plugins/memcached_bytes' && ln -s '/usr/share/munin/plugins/memcached_' '/etc/munin/plugins/memcached_commands' && ln -s '/usr/share/munin/plugins/memcached_' '/etc/munin/plugins/memcached_conns' && ln -s '/usr/share/munin/plugins/memcached_' '/etc/munin/plugins/memcached_evictions' && ln -s '/usr/share/munin/plugins/memcached_' '/etc/munin/plugins/memcached_items' && ln -s '/usr/share/munin/plugins/memcached_' '/etc/munin/plugins/memcached_memory'
+ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_bytes' && ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_commands' && ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_conns' && ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_evictions' && ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_items' && ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_memory'
 
 echo -e 'user bitrix\nenv.host unix:///tmp/memcached.sock\nenv.port 0\nenv.timescale 3\nenv.cmds get set delete incr decr touch\nenv.leitime -1' > /etc/munin/plugin-conf.d/memcache
 
