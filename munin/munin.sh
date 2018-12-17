9dd0bc4948c91106132292b8df4526d62044a1ca
YogSottot <7411302+YogSottot@users.noreply.github.com>
2018-12-17 15:07:56 +0300

    Add php-fpm to munin

--- munin/munin.sh
+++ munin/munin.sh
@@ -10,8 +10,18 @@ mkdir -p /etc/systemd/system/munin-node.service.d && echo -e '[Service]\nRestart
 wget https://raw.githubusercontent.com/mhwest13/Memcached-Munin-Plugin/master/memcached_ -N -O /usr/share/munin/plugins/memcached2_
 chmod +x /usr/share/munin/plugins/memcached2_
 
-ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_bytes' && ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_commands' && ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_conns' && ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_evictions' && ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_items' && ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_memory'
+rm -rf /etc/munin/plugins/memcached_*
+
+ln -fs '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_bytes' && ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_commands' && ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_conns' && ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_evictions' && ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_items' && ln -s '/usr/share/munin/plugins/memcached2_' '/etc/munin/plugins/memcached_memory'
 
 echo -e '[memcached_*]\nuser bitrix\nenv.host unix:///tmp/memcached.sock\nenv.port 0\nenv.timescale 3\nenv.cmds get set delete incr decr touch\nenv.leitime -1' > /etc/munin/plugin-conf.d/memcache
+
+yum install perl-FCGI-Client perl-Module-Pluggable
+
+wget https://raw.githubusercontent.com/munin-monitoring/contrib/master/plugins/php/php_fpm_process -N -P /etc/munin/plugins/
+
+chmod +x /etc/munin/plugins/php_fpm_process
+
+echo -e '[php_fpm_process]\nenv.sock /var/run/php-fpm/default.socket\nenv.path /status' > /etc/munin/plugin-conf.d/php_fpm.conf
 
 systemctl restart munin-node
