4da5f83c7f147129efb0099c84070727d619a401
YogSottot <7411302+YogSottot@users.noreply.github.com>
2019-05-15 22:49:56 +0300

    Update gitlab-runner repo

--- git/runner.sh
+++ git/runner.sh
@@ -3,8 +3,11 @@ set -e
 # use
 # curl -sL https://raw.githubusercontent.com/YogSottot/useful_scripts/master/git/runner.sh | bash
 
-curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.rpm.sh | bash
-yum -y install gitlab-ci-multi-runner
+curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.rpm.sh | bash
+yum -y install gitlab-runner
+
+# https://gitlab.com/gitlab-org/gitlab-runner/issues/2786
+/usr/share/gitlab-runner/post-install
 
 mkdir /etc/systemd/system/gitlab-runner.service.d
 echo -e '[Service]\nExecStart=\nExecStart=/usr/bin/gitlab-ci-multi-runner "run" "--working-directory" "/home/gitlab-runner" "--config" "/etc/gitlab-runner/config.toml" "--service" "gitlab-runner" "--syslog" "--user" "bitrix"'  >> /etc/systemd/system/gitlab-runner.service.d/override.conf
