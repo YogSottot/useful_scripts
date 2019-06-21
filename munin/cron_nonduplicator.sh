d04d4a4884e0bb3e76280a8a11a48d0328ff22ce
YogSottot <7411302+YogSottot@users.noreply.github.com>
2019-06-21 12:58:26 +0300

    Add cron nonduplicator template script

---
+++ munin/cron_nonduplicator.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e
+
+PIDFILE=/tmp/task1.pid
+if [ -f $PIDFILE ]
+then
+  PID=$(cat $PIDFILE)
+  ps -p $PID > /dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    echo "Process already running"
+    exit 1
+  else
+    ## Process not found assume not running
+    echo $$ > $PIDFILE
+    if [ $? -ne 0 ]
+    then
+      echo "Could not create PID file"
+      exit 1
+    fi
+  fi
+else
+  echo $$ > $PIDFILE
+  if [ $? -ne 0 ]
+  then
+    echo "Could not create PID file"
+    exit 1
+  fi
+fi
+
+# put your command here
+rm $PIDFILE
