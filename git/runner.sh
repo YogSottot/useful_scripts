af7014c8653d3143f7dc1a94d9d6f79b4a6eb5d4
YogSottot <7411302+YogSottot@users.noreply.github.com>
2019-08-25 14:28:52 +0300

    Update gitlab-runner to new repo

--- git/runner.sh
+++ git/runner.sh
@@ -3,16 +3,16 @@ set -e
 # use
 # curl -sL https://raw.githubusercontent.com/YogSottot/useful_scripts/master/git/runner.sh | bash
 
-#curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.rpm.sh | bash
-curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.rpm.sh | sudo bash
-#yum -y install gitlab-runner
-yum -y  gitlab-ci-multi-runner
+curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.rpm.sh | bash
+#curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.rpm.sh | sudo bash
+yum -y install gitlab-runner
+#yum -y install gitlab-ci-multi-runner
 
 # https://gitlab.com/gitlab-org/gitlab-runner/issues/2786
-#/usr/share/gitlab-runner/post-install
+/usr/share/gitlab-runner/post-install
 
 mkdir /etc/systemd/system/gitlab-runner.service.d
-echo -e '[Service]\nExecStart=\nExecStart=/usr/bin/gitlab-ci-multi-runner "run" "--working-directory" "/home/gitlab-runner" "--config" "/etc/gitlab-runner/config.toml" "--service" "gitlab-runner" "--syslog" "--user" "bitrix"'  >> /etc/systemd/system/gitlab-runner.service.d/override.conf
+echo -e '[Service]\nExecStart=\nExecStart=/usr/bin/gitlab-runner "run" "--working-directory" "/home/gitlab-runner" "--config" "/etc/gitlab-runner/config.toml" "--service" "gitlab-runner" "--syslog" "--user" "bitrix"'  >> /etc/systemd/system/gitlab-runner.service.d/override.conf
 chown -R bitrix:bitrix /home/gitlab-runner/
 
 systemctl daemon-reload
