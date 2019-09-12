e5e62f4c43cdff71b3a56cd295697d2e297a5f37
YogSottot <7411302+YogSottot@users.noreply.github.com>
2019-09-12 15:55:44 +0300

    Add firewalld zone for qrator

---
+++ av/qrator.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# use
+# curl -sL https://raw.githubusercontent.com/YogSottot/useful_scripts/master/av/qrator.sh | bash
+
+# open ports only for qrator upstream servers
+firewall-cmd --new-zone=qrator --permanent 
+firewall-cmd --reload
+firewall-cmd --permanent --zone=qrator --add-port=80/tcp 
+firewall-cmd --permanent --zone=qrator --add-port=443/tcp 
+firewall-cmd --permanent --zone=qrator --add-source=87.245.197.192
+firewall-cmd --permanent --zone=qrator --add-source=87.245.197.193
+firewall-cmd --permanent --zone=qrator --add-source=87.245.197.194
+firewall-cmd --permanent --zone=qrator --add-source=87.245.197.195
+firewall-cmd --permanent --zone=qrator --add-source=83.234.15.112
+firewall-cmd --permanent --zone=qrator --add-source=83.234.15.113
+firewall-cmd --permanent --zone=qrator --add-source=83.234.15.114
+firewall-cmd --permanent --zone=qrator --add-source=83.234.15.115
+firewall-cmd --permanent --zone=qrator --add-source=66.110.32.128
+firewall-cmd --permanent --zone=qrator --add-source=66.110.32.129
+firewall-cmd --permanent --zone=qrator --add-source=66.110.32.130
+firewall-cmd --permanent --zone=qrator --add-source=66.110.32.131
+firewall-cmd --permanent --zone=qrator --add-source=185.94.108.0/24
+firewall-cmd --reload
+firewall-cmd --zone=public --remove-port=443/tcp --permanent
+firewall-cmd --zone=public --remove-port=80/tcp --permanent
+firewall-cmd --reload
