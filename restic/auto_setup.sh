7bcb56523ffd0201406d25ad99dc665fdb2faa74
YogSottot <7411302+YogSottot@users.noreply.github.com>
2019-10-13 19:48:08 +0300

    Set monthly retention to 6m

--- restic/auto_setup.sh
+++ restic/auto_setup.sh
@@ -27,7 +27,7 @@ source /etc/bash_completion.d/restic
 
 #crontab -l | { cat; echo "30 1 * * * /opt/backup/restic/restic-wrapper.sh $1 $3 --exclude-file=/opt/backup/restic/exclude/$1.txt backup $2 > /dev/null 2>&1 || true && /opt/backup/restic/restic-wrapper.sh $1 $3 forget --prune \${keep_policy[@]} > /dev/null 2>&1 || true"; } | crontab -
 crontab -l | { cat; echo "30 1 * * * /opt/backup/restic/restic-wrapper.sh $1 $3 backup --exclude-file=/opt/backup/restic/exclude/$1.txt $2 > /dev/null 2>&1 || true"; } | crontab -
-crontab -l | { cat; echo "30 3 * * 7 /opt/backup/restic/restic-wrapper.sh $1 $3 forget --prune --keep-daily 14 --keep-weekly 4 --keep-monthly 2 > /dev/null 2>&1 || true"; } | crontab -
+crontab -l | { cat; echo "30 3 * * 7 /opt/backup/restic/restic-wrapper.sh $1 $3 forget --prune --keep-daily 14 --keep-weekly 4 --keep-monthly 6 > /dev/null 2>&1 || true"; } | crontab -
 crontab -l | { cat; echo "30 3 1 */2 * /opt/backup/restic/restic-wrapper.sh $1 $3 check > /dev/null 2>&1 || true"; } | crontab -
 
 echo 'do not forget to fill data in rc.file'
