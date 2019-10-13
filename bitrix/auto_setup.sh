600b482b506c80658b802e7612c93420bcad3a06
YogSottot <7411302+YogSottot@users.noreply.github.com>
2019-10-13 17:56:34 +0300

    Add hourly and dayly db backup scripts

--- bitrix/auto_setup.sh
+++ bitrix/auto_setup.sh
@@ -3,36 +3,39 @@ set -e
 # use
 # curl -sL https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/auto_setup.sh | bash -s -- /home/bitrix/www
 
-mkdir -p /opt/backup/db && cd /opt/backup/
+mkdir -p /opt/backup/db 
+cd /opt/backup/
 chmod 700 /opt/backup/
-wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/backup-db.php &&
-wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/backup_bitrixdb_native.sh &&
-wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/backup_bitrixdb.sh &&
-wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/rclone.sh &&
-wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/rclone_restore_upload.sh &&
-wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/backup_bitrix_files.sh &&
-wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/backup_configs.sh &&
-wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/test.php &&
-wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/percona-db.sh &&
-wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/percona-db-stream.sh &&
-wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/percona-restore-stream.sh &&
-wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/percona-restore.sh &&
-wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/percona-restore-single-db.sh &&
+#wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/backup-db.php
+#wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/backup_bitrixdb_native.sh
+wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/backup_bitrixdb_hourly.sh
+wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/backup_bitrixdb_daily.sh
+#wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/rclone.sh
+#wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/rclone_restore_upload.sh
+#wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/backup_bitrix_files.sh
+wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/backup_configs.sh
+#wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/test.php
+wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/percona-db.sh
+wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/percona-db-stream.sh
+wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/percona-restore-stream.sh
+wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/percona-restore.sh
+wget -N https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/percona-restore-single-db.sh
 
 chmod +x *.sh
-yum -y install https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.1.7-1.el7.x86_64.rpm
-yum -y install https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-utilities-1.6.5-1.el7.noarch.rpm
-yum -y install python-pip percona-xtrabackup-24 qpress pv percona-toolkit
+#yum -y install https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.1.7-1.el7.x86_64.rpm
+#yum -y install https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-utilities-1.6.5-1.el7.noarch.rpm
+yum -y install python-pip percona-xtrabackup-24 qpress pv percona-toolkit mailx
 pip install python-swiftclient --user
 
 # for restore upload directly from bitrix
-sudo -i -u bitrix bash << EOF
-pip install python-swiftclient --user
-EOF
-crontab -l | { cat; echo "30 */3 * * * /opt/backup/backup_bitrixdb_native.sh $1 > /dev/null 2>&1 || true" ; } | crontab -
-#crontab -l | { cat; echo "00 01 * * * /opt/backup/backup_upload.sh $1 > /dev/null 2>&1 || true"; } | crontab -
-crontab -l | { cat; echo "#20 */3 * * * /opt/backup/percona-db-stream.sh $1 $2 > /dev/null 2>&1 || true"; } | crontab -
-crontab -l | { cat; echo "20 1 * * * /opt/backup/backup_configs.sh $1 > /dev/null 2>&1 || true"; } | crontab -
-crontab -l | { cat; echo "10 1 * * * /opt/backup/backup_bitrix_files.sh $1 > /dev/null 2>&1 || true"; } | crontab -
+#sudo -i -u bitrix bash << EOF
+#pip install python-swiftclient --user
+#EOF
+#crontab -l | { cat; echo "30 */3 * * * /opt/backup/backup_bitrixdb_native.sh $1 > /dev/null 2>&1 || true" ; } | crontab -
+#crontab -l | { cat; echo "#20 */3 * * * /opt/backup/percona-db-stream.sh $1 $2 > /dev/null 2>&1 || true"; } | crontab -
+crontab -l | { cat; echo "05 0 * * * /opt/backup/backup_configs.sh $1 > /dev/null 2>&1 || true"; } | crontab -
+#crontab -l | { cat; echo "10 1 * * * /opt/backup/backup_bitrix_files.sh $1 > /dev/null 2>&1 || true"; } | crontab -
+crontab -l | { cat; echo "15 1 * * * /opt/backup/backup_bitrixdb_daily.sh $1 > /dev/null 2>&1 || true"; } | crontab -
+#crontab -l | { cat; echo "40 */3 * * * /opt/backup/backup_bitrixdb_hourly.sh $1 > /dev/null 2>&1 || true"; } | crontab -
 
-curl -sL https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/rclone.sh | bash -s -- $1
+#curl -sL https://raw.githubusercontent.com/YogSottot/useful_scripts/master/bitrix/rclone.sh | bash -s -- $1
---
+++ bitrix/backup_bitrixdb_hourly.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+set -e
+
+doc_root=$1
+name=$2
+if [ -z ${doc_root} ]; then
+	echo Usage: $0 /path/to/document/root [backup_name]
+	exit
+fi
+
+if [ -z ${name} ]; then
+	name=`/bin/hostname`
+fi
+
+dbconn=${doc_root}/bitrix/php_interface/dbconn.php
+
+readcfg() {
+	grep $1 ${dbconn} | sed 's/.*"\(.*\)".*/\1/'
+}
+
+host=`readcfg DBHost`
+username=`readcfg DBLogin`
+password=`readcfg DBPassword`
+database=`readcfg DBName`
+
+utf=`grep 'BX_UTF' ${dbconn} | grep true`
+
+if [ -z "$utf" ]; then
+	charset=cp1251
+else
+	charset=utf8
+fi
+
+#backup_dir=${doc_root}/bitrix/backup
+backup_dir=/opt/backup/backup
+
+if [ ! -e ${backup_dir} ]; then
+	mkdir -p ${backup_dir}
+fi
+
+
+
+
+function getValueFromINI() {
+	local sourceData=$1; local paramName=$2;
+	## 1. Get value "platform=%OUR_VALUE%"
+	## 2. Remove illegal characters
+	echo $(echo "$sourceData" | sed -n '/^'${paramName}'\ =\(.*\)$/s//\1/p' | tr -d "\r" | tr -d "\n");
+}
+
+function getValueFromINI2() {
+        local sourceData=$1; local paramName=$2;
+        ## 1. Get value "platform=%OUR_VALUE%"
+        ## 2. Remove illegal characters
+        echo $(echo "$sourceData" | sed -n '/^'${paramName}'\ =\(.*\)$/s//\1/p'  | tr -d "\r" | tr -d "\n" | tr -d "/");
+}
+
+sectionContent=$(sed -n '/^\[cloud\]/,/^\[/p' /opt/sMonit/config.ini | sed -e '/^\[/d' | sed -e '/^$/d');
+login=$(getValueFromINI "$sectionContent" "login");
+userkey=$(getValueFromINI "$sectionContent" "password");
+storage_dir=$(getValueFromINI2 "$sectionContent" "dir");
+
+cd ${doc_root} && \
+nice -n 19 ionice -c2 -n7 \
+mysqldump -e --add-drop-table --add-locks \
+--skip-lock-tables --single-transaction --quick \
+-h${host} -uroot --default-character-set=${charset} \
+${database} | pv -L 10m  | \
+nice -n 19 ionice -c2 -n7 gzip > ${backup_dir}/${name}.sql.gz > /tmp/backup_bitrixdb_hourly_log 2>&1 && \
+nice -n 19 ionice -c2 -n7 /root/.local/bin/swift -q -A https://auth.selcdn.ru -U ${login} -K ${userkey} upload -H "X-Delete-After: 129600" --object-name `date +%Y-%m-%d-%H:%M`_DB_Only_hourly/ ${storage_dir} ${backup_dir}/ >> /tmp/backup_bitrixdb_hourly_log 2>&1
+
+exitcode="$?"
+
+# output
+if [ "${exitcode}" -ne "0" ]; then
+    mailx -s "$(echo -e  "Backup bitrixdb hourly on ${hostname} is error\nContent-Type: text/plain; charset=UTF-8")" ${mail} < /tmp/backup_bitrixdb_hourly_log
+fi
+
+rm -rf ${backup_dir}/*
+exit 0
