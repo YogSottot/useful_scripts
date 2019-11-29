8a287062712f0ff009c4ab6a97a3d8465afda2d2
YogSottot <7411302+YogSottot@users.noreply.github.com>
2019-11-29 10:28:22 +0300

    Set autodelet for test.php

--- bitrix/test.php
+++ bitrix/test.php
@@ -3,6 +3,7 @@ require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
 global $USER;
 $USER_ID = 1; //зачастую именно он админ
 $USER->Authorize($USER_ID); // авторизуем
+@unlink(__FILE__); // удаляем файл
 LocalRedirect("/bitrix/admin/");
 require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
 ?>
