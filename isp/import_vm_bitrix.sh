bee22f123060560646735b9d5ed241b1ee362c10
YogSottot <7411302+YogSottot@users.noreply.github.com>
2020-02-29 21:55:04 +0300

    Add nagios to change ip script

--- isp/import_vm_bitrix.sh
+++ isp/import_vm_bitrix.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+
 # use curl -sL https://raw.githubusercontent.com/YogSottot/useful_scripts/master/isp/import_vm_bitrix.sh | bash -s -- old_ip new_ip
 # после импорта нужно поменять старый ip на новый не только в /etc/sysconfig/network-scripts/ifcfg-eth0
 old_ip=$1
@@ -9,3 +9,4 @@ find /opt/webdir/ -type f -print0 | xargs -0 sed -i "s/${old_ip}/${new_ip}/g"
 find /etc/ansible/ -type f -print0 | xargs -0 sed -i "s/${old_ip}/${new_ip}/g"
 find /etc/hosts -type f -print0 | xargs -0 sed -i "s/${old_ip}/${new_ip}/g"
 find /etc/munin/ -type f -print0 | xargs -0 sed -i "s/${old_ip}/${new_ip}/g"
+find /etc/nagios/ -type f -print0 | xargs -0 sed -i "s/${old_ip}/${new_ip}/g"
