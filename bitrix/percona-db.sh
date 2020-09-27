76776a276c9b19025303536c4db1462159483788
YogSottot <7411302+YogSottot@users.noreply.github.com>
2020-09-27 12:14:36 +0300

    Add https://nginx.viraptor.info/

--- bitrix/percona-db.sh
+++ bitrix/percona-db.sh
@@ -4,4 +4,4 @@ set -e
 rm -rf /opt/backup/db.prev
 mv /opt/backup/db /opt/backup/db.prev
 
-xtrabackup --backup --compress --target-dir=/opt/backup/db
+xtrabackup --backup --lock-ddl --compress --target-dir=/opt/backup/db
