2bf53f65144b0264afea36220ecd211f4ff1ffc0
YogSottot <7411302+YogSottot@users.noreply.github.com>
2020-11-05 12:50:20 +0300

    Add restic autoupdate

--- restic/auto_setup.sh
+++ restic/auto_setup.sh
@@ -17,17 +17,17 @@ wget https://raw.githubusercontent.com/YogSottot/useful_scripts/master/restic/ww
 wget https://raw.githubusercontent.com/YogSottot/useful_scripts/master/restic/www.txt -N -P /opt/backup/restic/exclude/
 chmod +x *.sh
 
-wget https://github.com/restic/restic/releases/download/v0.9.5/restic_0.9.5_linux_amd64.bz2
-bunzip2 restic_0.9.5_linux_amd64.bz2
-mv restic_0.9.5_linux_amd64 /usr/local/bin/restic
+wget https://github.com/restic/restic/releases/download/v0.10.0/restic_0.10.0_linux_amd64.bz2
+bunzip2 restic_0.10.0_linux_amd64.bz2
+mv restic_0.10.0_linux_amd64 /usr/local/bin/restic
 chmod +x /usr/local/bin/restic
 restic self-update
 
 restic generate --bash-completion /etc/bash_completion.d/restic
 source /etc/bash_completion.d/restic
 
-#crontab -l | { cat; echo "30 1 * * * /opt/backup/restic/restic-wrapper.sh $1 $3 --exclude-file=/opt/backup/restic/exclude/$1.txt backup $2 > /dev/null 2>&1 || true && /opt/backup/restic/restic-wrapper.sh $1 $3 forget --prune \${keep_policy[@]} > /dev/null 2>&1 || true"; } | crontab -
-crontab -l | { cat; echo "30 1 * * * /opt/backup/restic/restic-wrapper.sh $1 $3 backup --exclude-file=/opt/backup/restic/exclude/$1.txt $2 > /dev/null 2>&1 && /opt/backup/restic/restic-diff.sh $1 $3 > /dev/null 2>&1 || true"; } | crontab -
+#crontab -l | { cat; echo "30 1 * * *  /opt/backup/restic/restic-wrapper.sh $1 $3 --exclude-file=/opt/backup/restic/exclude/$1.txt backup $2 > /dev/null 2>&1 || true && /opt/backup/restic/restic-wrapper.sh $1 $3 forget --prune \${keep_policy[@]} > /dev/null 2>&1 || true"; } | crontab -
+crontab -l | { cat; echo "30 1 * * * /usr/local/bin/restic self-update ;  /opt/backup/restic/restic-wrapper.sh $1 $3 backup --exclude-file=/opt/backup/restic/exclude/$1.txt $2 > /dev/null 2>&1 && /opt/backup/restic/restic-diff.sh $1 $3 > /dev/null 2>&1 || true"; } | crontab -
 crontab -l | { cat; echo "30 3 * * 7 /opt/backup/restic/restic-wrapper.sh $1 $3 forget --prune --keep-daily 14 --keep-weekly 4 --keep-monthly 6 > /dev/null 2>&1 || true"; } | crontab -
 crontab -l | { cat; echo "30 3 1 */2 * /opt/backup/restic/restic-wrapper.sh $1 $3 check > /dev/null 2>&1 || true"; } | crontab -
 
