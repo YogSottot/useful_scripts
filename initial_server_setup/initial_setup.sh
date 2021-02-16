5680e091d9a4b573b88645f605979ccecfe68971
YogSottot <7411302+YogSottot@users.noreply.github.com>
2021-02-16 10:05:59 +0300

    Add deps for img converting

--- initial_server_setup/initial_setup.sh
+++ initial_server_setup/initial_setup.sh
@@ -12,7 +12,8 @@ set -e
 # find -type f -name "*.png" -exec optipng -o7 {} \;
 
 # install useful programms
-yum -y install --enablerepo=epel wget byobu chrony net-tools certbot ncdu iotop htop bind-utils traceroute mc bash-completion bash-completion-extras yum-utils nano tmux deltarpm jpegoptim optipng ImageMagick php-pecl-imagick mysqltuner
+yum -y install --enablerepo=epel wget byobu chrony net-tools certbot ncdu iotop htop bind-utils traceroute mc bash-completion bash-completion-extras yum-utils nano tmux deltarpm jpegoptim optipng ghostscript gifsicle libwebp-tools ImageMagick php-pecl-imagick mysqltuner nodejs
+npm install -g svgo
 
 # install liquidprompt
 #cd /opt/ && git clone https://github.com/nojhan/liquidprompt
