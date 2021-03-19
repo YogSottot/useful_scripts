dc6f0f3e92458c0f94048fcd723a3b56e6a6d216
YogSottot <7411302+YogSottot@users.noreply.github.com>
2021-03-19 17:55:57 +0300

    Add --lock-ddl for percona_inc

--- bitrix/percona-db-inc.sh
+++ bitrix/percona-db-inc.sh
@@ -2,7 +2,7 @@
 set -e
 
 mkdir -p /opt/backup/inc/
-# delete inc older then 36 hours
-/usr/bin/find /opt/backup/inc/ -mindepth 1 -maxdepth 1 -type d -mmin +2160 -exec rm -rf {} \;
+# delete inc older then 15 hours
+/usr/bin/find /opt/backup/inc/ -mindepth 1 -maxdepth 1 -type d -mmin +900 -exec rm -rf {} \;
 # make inc backup for the current base
-xtrabackup --backup --compress --target-dir=/opt/backup/inc/`date +%Y-%m-%d-%H:%M` --incremental-basedir=/opt/backup/db
+xtrabackup --backup --lock-ddl --compress --target-dir=/opt/backup/inc/`date +%Y-%m-%d-%H:%M` --incremental-basedir=/opt/backup/db
