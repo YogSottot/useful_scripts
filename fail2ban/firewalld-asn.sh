5ed57ef27e68f870898e367053f9a63fc273266d
YogSottot <7411302+YogSottot@users.noreply.github.com>
2021-12-18 22:17:09 +0300

    Add deletion of old ipset for ASN-blocker for firewalld

--- fail2ban/firewalld-asn.sh
+++ fail2ban/firewalld-asn.sh
@@ -9,6 +9,20 @@ mkdir -p /tmp/ASN/
 
 for ASN in "$@"
 do
+## Remove and delete ${ASN}-4 ipset if it exists
+if firewall-cmd --permanent --get-ipsets | grep -q "${ASN}-4"; then
+  firewall-cmd --permanent --zone=drop --remove-source=ipset:${ASN}-4
+  firewall-cmd --permanent --delete-ipset=${ASN}-4
+  firewall-cmd --reload
+fi
+
+## Remove and delete ${ASN}-6 ipset if it exists
+if firewall-cmd --permanent --get-ipsets | grep -q "${ASN}-6"; then
+  firewall-cmd --permanent --zone=drop --remove-source=ipset:${ASN}-6
+  firewall-cmd --reload
+  firewall-cmd --permanent --delete-ipset=${ASN}-6
+fi
+
 # get list from https://asn.ipinfo.app
 wget https://asn.ipinfo.app/api/download/list/AS${ASN} -O /tmp/ASN/${ASN}
 # sort ipv4 and ipv6
