56094c14e438150507cf5305ecac47b8d9c92f53
YogSottot <7411302+YogSottot@users.noreply.github.com>
2022-04-11 15:40:35 +0300

    Update update_db

--- bitrix/update_db.sh
+++ bitrix/update_db.sh
@@ -16,6 +16,13 @@ mysql --execute="DELETE FROM ${database}.b_lang_domain WHERE LID='s1';"
 # Доменное имя: (список доменных имен, каждое в новой строке) для сайта s1 / s2 / s3
 mysql --execute="INSERT INTO ${database}.b_lang_domain (LID, domain) VALUES ('s1','dev.example.tld'),('s1','dev2.example.tld'),('s2','dev3.example.tld'),('s2','dev4.example.tld'),('s3','dev5.example.tld'),('s3','dev2.example.org');"
 
+# отключение pull/push-сервера
+mysql --execute="UPDATE ${database}.b_option SET VALUE='N' WHERE b_option.MODULE_ID='pull' AND b_option.NAME='nginx';"
+mysql --execute="UPDATE ${database}.b_option SET VALUE='N' WHERE b_option.MODULE_ID='pull' AND b_option.NAME='push';"
+# set pull-server on server
+mysql --execute="UPDATE ${database}.b_option SET VALUE='personal' WHERE b_option.MODULE_ID='pull' AND b_option.NAME='server_mode';"
+mysql --execute="UPDATE ${database}.b_option SET VALUE='BRfYy36Yjct' WHERE b_option.MODULE_ID='pull' AND b_option.NAME='signature_key';"
+
 # mysql --execute="select * from sitemanager.b_lang"
 # mysql --execute="select * from sitemanager.b_lang_domain"
 
