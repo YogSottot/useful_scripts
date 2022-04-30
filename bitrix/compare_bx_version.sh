16ba41c3f4bc121de0147a973a758f5196f8d79a
YogSottot <7411302+YogSottot@users.noreply.github.com>
2022-04-30 08:00:24 +0300

    Add compare_bx_version.sh

---
+++ bitrix/compare_bx_version.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+ssh_host="${1}"
+source_dir="${2}"
+target_dir="${3}"
+
+source_version=`ssh ${ssh_host} "grep -m 1 SM_VERSION ${source_dir}/bitrix/modules/main/classes/general/version.php | tr -dc '0-9.'"`
+target_version=`grep -m 1 SM_VERSION ${target_dir}/bitrix/modules/main/classes/general/version.php | tr -dc '0-9.'`
+
+echo source_version: "${source_version}"
+echo target_version: "${target_version}"
+
+
+if [ "${source_version}" == "${target_version}" ]
+then
+    echo "Bitrix version identical"
+else
+    echo "Bitrix version different!"
+    echo "Update your source!"
+    echo "DB download is stopped!"
+    exit 1
+fi
---
+++ bitrix/compare_bx_version_multitarget.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+ssh_host="${1}"
+source_dir="${2}"
+target1_dir="${3}"
+target2_dir="${4}"
+
+source_version=`ssh ${ssh_host} "grep -m 1 SM_VERSION ${source_dir}/bitrix/modules/main/classes/general/version.php | tr -dc '0-9.'"`
+target1_version=`grep -m 1 SM_VERSION ${target1_dir}/aliev.murad-semicvetic.com.s-webs.ru/bitrix/modules/main/classes/general/version.php | tr -dc '0-9.'`
+target2_version=`grep -m 1 SM_VERSION ${target2_dir}/bitrix/modules/main/classes/general/version.php | tr -dc '0-9.'`
+
+echo source_version: "${source_version}"
+echo target1_version: "${target1_version}"
+echo target2_version: "${target2_version}"
+
+
+if [ "${source_version}" == "${target1_version}" -a "${source_version}" == "${target2_version}" ]
+then
+    echo "Bitrix version identical"
+else
+    echo "Bitrix version different"
+    echo "Update your source!"
+    echo "DB download is stopped!"
+    exit 1
+fi
