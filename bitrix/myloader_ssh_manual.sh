d433f3ec2850f87b2a6e82faecb92a02a32c9d69
YogSottot <7411302+YogSottot@users.noreply.github.com>
2022-05-25 12:09:43 +0300

    Add sample for full sync (stage)

--- bitrix/myloader_ssh_manual.sh
+++ bitrix/myloader_ssh_manual.sh
@@ -26,9 +26,10 @@ rm -rf /opt/backup/mydumper
 printf "Start update db settings\n"
 /opt/backup/update_db.sh ${target_dir}
 
-# clean mysql cache
-printf "Clean mysql cache\n"
+printf "Remove cache dirs\n"
 rm -rf ${target_dir}/bitrix/managed_cache/*
+rm -rf ${target_dir}/bitrix/cache/*
+rm -rf ${target_dir}/bitrix/stack_cache/*
 
 # "if -e” statement, produces a “true” output if any specified file exists
 # “if -s” operator returns true if a file exists and also if it is not empty
@@ -44,4 +45,10 @@ fi
 # rsync upload
 printf "Start rsync upload\n"
 rsync -a ${source_ssh_host}:${source_dir}/upload/ ${target_dir}/upload/ --delete
+# full sync for stage
+# rsync --exclude '/robots.txt' --exclude '/bitrix/cache/*' --exclude '/bitrix/backup/*' --exclude '/bitrix/managed_cache/*'  --exclude '/bitrix/stack_cache/*' --exclude '/bitrix/html_pages/*' --exclude '/bitrix/.settings.php' --exclude '/bitrix/.settings_extra.php' --exclude '/bitrix/php_interface/dbconn.php'  -a ${source_ssh_host}:${source_dir}/ ${target_dir}/ --delete
+# full sync for ext_site
+# rsync --exclude '/robots.txt'  --exclude '/bitrix/' --exclude '/images/'  --exclude '/upload/' -a ${source_ssh_host}:/home/bitrix/ext_www/domain.tld/ /home/bitrix/ext_www/domain.tld/ --delete
 printf "Rsync upload finished!\n"
+
+printf "Sync finished succesfull!\n"
--- bitrix/percona_ssh_sample.sh
+++ bitrix/percona_ssh_sample.sh
@@ -23,9 +23,29 @@ printf "Start mysql_upgrade\n"
 printf "Update db settings after restore\n"
 /opt/backup/update_db.sh ${target_dir}
 
-printf "Remove managed_cache\n"
+printf "Remove cache dirs\n"
 rm -rf ${target_dir}/bitrix/managed_cache/*
+rm -rf ${target_dir}/bitrix/cache/*
+rm -rf ${target_dir}/bitrix/stack_cache/*
 
+# "if -e” statement, produces a “true” output if any specified file exists
+# “if -s” operator returns true if a file exists and also if it is not empty
+# "if -d” operator returns true if your file is a directory
+# “if -h” operator returns true if your file is a symbolic link
+# “if -r” operator will be true if the current user can read the file
+# “if -O” operator will be true if the current user owns the file
+if [ -s /etc/sysconfig/memcached ]; then
+    printf "Restart memcached\n"
+    systemctl restart memcached
+fi
+
+# rsync upload
 printf "Start rsync upload\n"
 rsync -a ${source_ssh_host}:${source_dir}/upload/ ${target_dir}/upload/ --delete
+# full sync for stage
+# rsync --exclude '/robots.txt' --exclude '/bitrix/cache/*' --exclude '/bitrix/backup/*' --exclude '/bitrix/managed_cache/*'  --exclude '/bitrix/stack_cache/*' --exclude '/bitrix/html_pages/*' --exclude '/bitrix/.settings.php' --exclude '/bitrix/.settings_extra.php' --exclude '/bitrix/php_interface/dbconn.php'  -a ${source_ssh_host}:${source_dir}/ ${target_dir}/ --delete
+# full sync for ext_site
+# rsync --exclude '/robots.txt'  --exclude '/bitrix/' --exclude '/images/'  --exclude '/upload/' -a ${source_ssh_host}:/home/bitrix/ext_www/domain.tld/ /home/bitrix/ext_www/domain.tld/ --delete
 printf "Rsync upload finished!\n"
+
+printf "Sync finished succesfull!\n"
