df459b0b9e28f2d701f79bde282cc860e0cbd8cd
YogSottot <7411302+YogSottot@users.noreply.github.com>
2022-06-30 08:35:37 +0300

    Add block ip in nginx

---
+++ nginx/bx_ext_ip.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# use
+#  curl -sL https://raw.githubusercontent.com/YogSottot/useful_scripts/master/nginx/bx_ext_ip.sh | bash -s 
+
+ip=`ip -4  addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`
+
+cat <<EOT > /etc/nginx/bx/site_avaliable/bx_ext_ip.conf
+server {
+    listen 80;
+    listen 443  ssl http2;
+    server_name ${ip};
+    include bx/conf/ssl.conf;
+    deny all;
+}
+EOT
+
+cd /etc/nginx/bx/site_enabled/ && \
+ln -s ../site_avaliable/bx_ext_ip.conf . && \
+nginx -t && \
+systemctl reload nginx
