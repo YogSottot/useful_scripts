5cc01577f12ba8e0d8dcf405f69734967be8dc80
YogSottot <7411302+YogSottot@users.noreply.github.com>
2023-12-07 14:10:13 +0600

    add lsof

--- initial_server_setup/initial_setup.sh
+++ initial_server_setup/initial_setup.sh
@@ -12,7 +12,7 @@ set -eo pipefail
 # find -type f -name "*.png" -exec optipng -o7 {} \;
 
 # install useful programms
-yum -y install --enablerepo=epel wget byobu chrony net-tools certbot ncdu iotop htop bind-utils traceroute mc bash-completion bash-completion-extras yum-utils nano tmux deltarpm jpegoptim optipng libwebp-tools ImageMagick php-pecl-imagick mysqltuner smem
+yum -y install --enablerepo=epel wget byobu chrony net-tools certbot ncdu iotop htop bind-utils traceroute mc bash-completion bash-completion-extras yum-utils nano tmux deltarpm jpegoptim optipng libwebp-tools ImageMagick php-pecl-imagick mysqltuner smem lsof
 # gifsicle ghostscript nodejs
 # npm install -g svgo
 
